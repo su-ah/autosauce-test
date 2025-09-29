665b24cd665b99e74a93b0459f48500e445f8878
git-commit-rm-rf <ray.nguyen@mail.utoronto.ca>
2025-09-29 16:37:55 -0400

    Basic draft for material class (#27)
    * draft for material class
    
    * v2 design. Uses textures instead of pure colors
    
    * fixed include error
    
    * should not compile

---
+++ include/modeling/material.hpp
@@ -0,0 +1,145 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <assimp/scene.h>
+#include "assimp/material.h"
+#include <memory>
+#include <vector>
+struct Texture {
+    const std::unique_ptr<const uint8_t> data;
+    const uint32_t width;
+    const uint32_t height;
+    const uint32_t n_channels;
+    const uint32_t id;
+
+    Texture(
+        std::unique_ptr<const uint8_t> data,
+        uint32_t width,
+        uint32_t height,
+        uint32_t n_channels,
+        uint32_t id
+    ): 
+        data(std::move(data)),
+        width(width),
+        height(height),
+        n_channels(n_channels),
+        id(id) {}
+
+    Texture() = delete;
+    ~Texture() = default;
+    
+private:
+    // no copy
+    Texture(const Texture&) = delete;
+    Texture& operator=(const Texture&) = delete;
+    
+    // no move
+    Texture(Texture&&) = delete;
+    Texture& operator=(Texture&&) = delete;
+};
+
+// A Material 
+struct Material {
+    // name of the material
+    const std::string name;
+
+    // aiTextureType_BASE_COLOR
+    const Texture &base_color;
+
+    // aiTextureType_NORMAL_CAMERA
+    const Texture &normal_camera;
+
+    // aiTextureType_EMISSION_COLOR
+    const Texture &emission_color;
+
+    // aiTextureType_METALNESS
+    const Texture &metalness;
+
+    // aiTextureType_DIFFUSE_ROUGHNESS
+    const Texture &diffuse_roughness;
+
+    // aiTextureType_AMBIENT_OCCLUSION
+    const Texture &ambient_occlusion;
+    
+    
+    Material(
+        std::string name,
+        Texture &base_color,
+        Texture &normal_camera,
+        Texture &emission_color,
+        Texture &metalness,
+        Texture &diffuse_roughness,
+        Texture &ambient_occlusion
+    ):
+        name(std::move(name)),
+        base_color(base_color),
+        normal_camera(normal_camera),
+        emission_color(emission_color),
+        metalness(metalness),
+        diffuse_roughness(diffuse_roughness),
+        ambient_occlusion(ambient_occlusion)
+    {};
+    ~Material() = default;
+
+    // constructs a `Material` from an imported aiMaterial
+    static Material from_aiMaterial(aiMaterial *material);
+
+private:
+    // prevent copies
+    Material(const Material&) = delete;
+    Material& operator=(const Material&) = delete;
+
+    // prevent moving
+    Material(Material&&) = delete;
+    Material& operator=(Material&&) = delete;
+
+};
+
+// A unique index into `MaterialManager` 
+// to retrieve a `Material` 
+struct MaterialHandle {
+public:
+    friend class MaterialManager;
+
+    // creates a `MaterialHandle` without checking if its valid
+    static MaterialHandle new_unchecked(size_t id);
+
+private:
+    size_t id;
+    MaterialHandle(size_t id): id(id) {}
+};
+
+// A Context managing all imported `Material`s
+// that exist within a scene
+class MaterialManager {
+public:
+
+    // constructor from an imported scene
+    explicit MaterialManager(aiScene *scene);
+    ~MaterialManager() = default;
+
+    // prevent copies
+    MaterialManager(const MaterialManager&) = delete;
+    MaterialManager& operator=(const MaterialManager&) = delete;
+    
+    // allow moves
+    MaterialManager(MaterialManager&&) = default;
+    MaterialManager& operator=(MaterialManager&&) = default;
+
+    // constructor from a scene
+    static MaterialManager from_aiScene(aiScene scene);
+
+    // returns a material from its unique handle
+    const Material& get(MaterialHandle handle) const noexcept;
+
+    // returns a reference to a material given its name
+    // throws std::out_of_range the material is not found.
+    const Material& find(std::string name) const;
+
+
+private:
+    // list of all materials in a scene 
+    std::vector<Material> materials;
+
+    // list of all textures in a scene
+    std::vector<Texture> textures;
+};
---
+++ src/modeling/material.cpp
@@ -0,0 +1,29 @@
+#include "modeling/material.hpp" 
+#include <iostream> 
+
+Material Material::from_aiMaterial(aiMaterial *material) {
+    throw std::runtime_error("todo for model loading");
+}
+
+MaterialManager::MaterialManager(aiScene *scene) {
+    // load all textures first into this.textures
+    // load materials which then reference the loaded textures
+    throw std::runtime_error("todo for model loading");
+}
+
+MaterialHandle MaterialHandle::new_unchecked(size_t id) {
+    return MaterialHandle{ id };
+}
+
+const Material& MaterialManager::get(MaterialHandle handle) const noexcept {
+    return this->materials[handle.id];
+}
+
+const Material& MaterialManager::find(std::string name) const {
+    for (auto it = this->materials.begin(); it != this->materials.end(); ++it) {
+        if (it->name == name) {
+            return *it;
+        }
+    }
+    throw std::out_of_range(name + " is not a registered material");
+}

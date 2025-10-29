03ab2049647c052f12563aa6cc18e32ea35438c5
su-ah <emmrald4@gmail.com>
2025-10-28 23:49:10 -0400

    implemented stuff

--- include/shared/Scene.hpp
+++ include/shared/Scene.hpp
@@ -8,8 +8,16 @@
 
 class Scene {
 private:
+    static Scene *instance;
+
     std::vector<Object> objects;
+    std::vector<Object> lights; // change to Light class later
 public:
+
+    static Scene *getInstance() {
+        return Scene::instance;
+    }
+
     Scene();
     Scene(std::string &filename);
     ~Scene();
@@ -17,6 +25,10 @@ public:
     void load();
     void unload();
     void update(double timestep);
+
+    std::vector<Object> getLights(); // change to Light class later
+    void addLight(const Object &light); // change to Light class later
 };
+Scene *Scene::instance; // ??? i hate c++ -emmy
 
 #endif
